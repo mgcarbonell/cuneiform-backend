32b930b5fa34e3d206539f49edfc1aca076b619f
mgcarbonell <mgcarbonell@gmail.com>
2020-11-17 19:55:59 -0800

    Entry crud functionality boiler

--- controllers/entry.js
+++ controllers/entry.js
@@ -1,1 +1,21 @@
 const db = require('../models')
+
+// working
+const entry = (req, res) => {
+  db.entry.findAll().then((foundEntries) => {
+    if(!foundEntries) return res.json({
+      message: 'No Entries have been found.'
+    })
+
+    res. status(200).json({entries: foundEntries})
+  }) 
+}
+
+const show = (req, res) => {
+  db.entry.findByPk(req.params.id).then((foundEntry) => {
+    if (!foundEntry) return res.json({
+      message: 'No entry with that ID has been found.'
+    })
+    res.status(200).json({entry: foundEntry})
+  })
+}
